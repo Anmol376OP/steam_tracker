42d95d1bd8419036064214622f58a48bc36df2c0
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-05-16 00:39:55 +0530

    Changes

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -7,22 +7,20 @@ export default function SideNavbar(props) {
     }
     const [playerCount, setPlayerCount] = useState(0);
     const fetchPlayerCount = async (appid) => {
-        const response = await fetch(`http://localhost:8000/player_counts/${appid}`);
-        const data = await response.json();
-        setPlayerCount(data.current_player_count)
-        console.log(playerCount)
+        const response = await fetch(`http://localhost:8000/player_counts/${appid}`).then(async (e) => await e.json()).then(async (e) => setPlayerCount(e.current_player_count)).then((e) => console.log(playerCount))
+
     };
 
 
 
     let url = 'https://cdn-icons-png.flaticon.com/512/5087/5087379.png'
     return (
-        <div className="nav-body" >
-            <div className="nav-profile" onClick={() => HandleClick()}>
-                <div className="profile-image" style={{ backgroundImage: `url('${url}')` }} onClick={() => fetchPlayerCount(570)}></div>
+        <div className="nav-body" onClick={() => HandleClick()}>
+            <div className="nav-profile" >
+                <div className="profile-image" style={{ backgroundImage: `url('${url}')` }} onClick={() => fetchPlayerCount(440)}></div>
                 <div className={props.flex === 5 ? 'profile-name transition-in ml10 wrap' : 'profile-name dispHidden transition-out'}>
-                    <span className="heading">Thunder376</span>
-                    <span className="sub-heading">Anmol</span>
+                    <span className="heading" onClick={() => fetchPlayerCount(570)}>Thunder376</span>
+                    <span className="sub-heading" onClick={() => fetchPlayerCount(730)}>Anmol</span>
                 </div>
             </div>
         </div>
