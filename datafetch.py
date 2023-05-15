6a6665655faad9730c01029c31abfa2b2a9d1ac7
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-05-16 00:22:32 +0530

    fastApi backend

--- datafetch.py
+++ datafetch.py
@@ -1,27 +1,22 @@
-import requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 
-def fetch_player_count(appid):
-    url = f"http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data["response"]["player_count"]
-    else:
-        print(f"Failed to fetch player count. Status code: {response.status_code}")
-        return None
-
-appid = 578080
-player_count = fetch_player_count(appid)
-if player_count is not None:
-    print(f"Current player count for app ID {appid}: {player_count}")
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
 )
 
-@app.get("/api")
-def read_root():
-    return [{"player_count" : player_count}]
+
+@app.get('/player_counts/{appid}')
+async def get_player_counts(appid: int):
+    url = f"http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}"
+    response = requests.get(url)
+    data = response.json()
+    player_count = data['response']['player_count']
+    return {'current_player_count': player_count}
--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -5,16 +5,24 @@ export default function SideNavbar(props) {
     const HandleClick = () => {
         props.flex === 5 ? props.setFlex(12) : props.setFlex(5)
     }
-    const [playerCount, setPlayerCount] = useState(5);
+    const [playerCount, setPlayerCount] = useState(0);
+    const fetchPlayerCount = async (appid) => {
+        const response = await fetch(`http://localhost:8000/player_counts/${appid}`);
+        const data = await response.json();
+        setPlayerCount(data.current_player_count)
+        console.log(playerCount)
+    };
+
+
 
     let url = 'https://cdn-icons-png.flaticon.com/512/5087/5087379.png'
     return (
-        <div className="nav-body" onClick={() => HandleClick()}>
-            <div className="nav-profile">
-                <div className="profile-image" style={{ backgroundImage: `url('${url}')` }}></div>
+        <div className="nav-body" >
+            <div className="nav-profile" onClick={() => HandleClick()}>
+                <div className="profile-image" style={{ backgroundImage: `url('${url}')` }} onClick={() => fetchPlayerCount(570)}></div>
                 <div className={props.flex === 5 ? 'profile-name transition-in ml10 wrap' : 'profile-name dispHidden transition-out'}>
                     <span className="heading">Thunder376</span>
-                    <span className="sub-heading">{playerCount}</span>
+                    <span className="sub-heading">Anmol</span>
                 </div>
             </div>
         </div>
